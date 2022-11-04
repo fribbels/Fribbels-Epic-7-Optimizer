b78ee941e34a5d428d96b7c1d647eaf094851dea
MFerreira <jmferreirab@gmail.com>
2022-11-03 21:27:44 -0500

    Resolves #50. Force sniff all interfaces

--- data/py/scanner.py
+++ data/py/scanner.py
@@ -53,25 +53,23 @@ def check_packet(packet, index):
                 # if 'F' in packet[TCP].flags:
                 #     try_buffer(currAck)
 
-def thread_sniff(i, index):
-    try:
-        sniff(iface=i, prn=lambda x: check_packet(x, index), filter="tcp and ( port 3333 )", session=TCPSession)
-    except:
-        pass
-
-index = 0
-for i in list(conf.ifaces.data.values()):
-    try:
-        x = threading.Thread(target=thread_sniff, args=(i, index,))
-        x.daemon = True;
-        x.start()
-
-        index = index + 1
-    except:
-        pass
-
 def terminate():
     os._exit(0)
+
+def thread_sniff():
+    try:
+        # EpicSeven traffic was confirmed to travel over tcp port 3333 via Wireshark
+        # Omitting sniff() iface parameter to force all interfaces to be sniffed.
+        # This may lead to more processing but prevents needing to specify an network interface manually in some cases.
+        sniff(prn=lambda x: check_packet(x), filter="tcp and ( port 3333 )", session=TCPSession)
+    except:
+        pass
+
+
+x = threading.Thread(target=thread_sniff)
+x.daemon = True;
+x.start()
+
 
 t = threading.Timer(3600.0, terminate)
 t.start()
